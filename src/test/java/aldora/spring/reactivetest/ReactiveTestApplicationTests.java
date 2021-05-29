c2ca991a4212b1e26b84ac94353ca1ff8c9437f0
ShakalakaB <yunjie989@gmail.com>
2021-05-29 19:15:16 +0800

    Init project

---
+++ src/test/java/aldora/spring/reactivetest/ReactiveTestApplicationTests.java
@@ -0,0 +1,13 @@
+package aldora.spring.reactivetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
